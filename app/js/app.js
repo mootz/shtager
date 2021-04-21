4b144009eaf59f27e578c3ff6dc320fc92df5342
mootz <peeops@gmail.com>
2021-04-21 08:14:40 +0500

    upd

--- app/js/app.js
+++ app/js/app.js
@@ -4,17 +4,75 @@ $(document).ready(function () {
     const body = document.querySelector('body');
     const menuLinks = document.querySelectorAll('.menu__link')
 
-    menuButton.addEventListener('click', function() {
+    menuButton.addEventListener('click', function () {
         this.classList.toggle('active');
         menu.classList.toggle('active');
         body.classList.toggle('menu-active');
     });
 
     menuLinks.forEach(e => {
-        e.addEventListener('click', function() {
+        e.addEventListener('click', function () {
             menuButton.classList.toggle('active');
             menu.classList.toggle('active');
             body.classList.toggle('menu-active');
         })
     })
+
+    if (document.querySelector('.main-slider__container')) {
+        const mainSlider = new Swiper('.main-slider__container', {
+            loop: true,
+            autoplay: {
+                delay: 3000
+            },
+            speed: 1000,
+            spaceBetween: 40,
+
+            // If we need pagination
+            pagination: {
+                el: '.main-slider__pagination',
+                clickable: true
+            },
+
+        });
+    }
+
+    if (document.querySelector('.about-slider__container')) {
+        const aboutSlider = new Swiper('.about-slider__container', {
+            loop: true,
+            autoplay: {
+                delay: 3000
+            },
+            speed: 1000,
+            spaceBetween: 32,
+            slidesPerView: 2,
+
+            breakpoints: {
+                0: {
+                    spaceBetween: 10,
+                    slidesPerView: 1
+                },
+                400: {
+                    spaceBetween: 20
+                },
+                576: {
+                    slidesPerView: 2
+                },
+                768: {
+                    spaceBetween: 32
+                }
+            }
+        });
+
+        $().fancybox({
+            selector: '.about-slider__slide',
+            backFocus: false,
+            loop: true,
+            observer: true,
+            infobar: false,
+            observeParents: true,
+            buttons: [
+                "close"
+            ]
+        });
+    }
 });
